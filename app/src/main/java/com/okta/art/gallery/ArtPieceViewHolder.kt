f078c8eef88d49efc4bf88e300a71bd7ef285fd9
Jay Newstrom <jaynewstrom@gmail.com>
2020-11-18 10:35:51 -0600

    More design tweaks.

--- app/src/main/java/com/okta/art/gallery/ArtPieceViewHolder.kt
+++ app/src/main/java/com/okta/art/gallery/ArtPieceViewHolder.kt
@@ -9,12 +9,14 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.okta.art.R
 import com.okta.art.database.ArtPiece
+import java.text.SimpleDateFormat
+import java.util.*
 
 internal class ArtPieceViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.art_piece_item, parent, false)
 ) {
-    private val nameTextView = itemView.findViewById<TextView>(R.id.title_text_view)
-    private val userTextView = itemView.findViewById<TextView>(R.id.user_text_view)
+    private val nameTextView = itemView.findViewById<TextView>(R.id.title_text_view)!!
+    private val userTextView = itemView.findViewById<TextView>(R.id.user_text_view)!!
     private val imageView = itemView.findViewById<ImageView>(R.id.image_view)
     private var artPiece: ArtPiece? = null
 
@@ -28,8 +30,22 @@ internal class ArtPieceViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
         if (artPiece == null) return
 
         nameTextView.text = artPiece.title
-        userTextView.text = artPiece.user
+        userTextView.text = userTextView.resources.getString(
+            R.string.upload_by_on,
+            artPiece.user,
+            artPiece.uploadDate.formattedForDisplay()
+        )
         val rawResource = artPiece.file.split(".")[0]
         imageView.load(Uri.parse("android.resource://com.okta.art/raw/$rawResource"))
     }
+}
+
+private val simpleDateFormat = object : ThreadLocal<SimpleDateFormat>() {
+    override fun initialValue(): SimpleDateFormat {
+        return SimpleDateFormat("MM/dd/yy", Locale.US)
+    }
+}
+
+private fun Date.formattedForDisplay(): String {
+    return simpleDateFormat.get().format(this)
 }
