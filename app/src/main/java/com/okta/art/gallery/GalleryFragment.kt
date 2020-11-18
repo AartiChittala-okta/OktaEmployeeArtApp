c8e16c915112af67f4abab842c28c03d40fa33c7
Jay Newstrom <jaynewstrom@gmail.com>
2020-11-18 10:53:11 -0600

    Use base fragment.

--- app/src/main/java/com/okta/art/gallery/GalleryFragment.kt
+++ app/src/main/java/com/okta/art/gallery/GalleryFragment.kt
@@ -4,20 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.okta.art.base.BaseFragment
 import com.okta.art.databinding.FragmentGalleryBinding
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-internal class GalleryFragment : Fragment() {
-    private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
+internal class GalleryFragment : BaseFragment<FragmentGalleryBinding>() {
     private val viewModel: GalleryViewModel by viewModels()
 
     override fun onCreateView(
